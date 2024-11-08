24cc4efe7f1d19490c089d590daff7fb2e0f5f83
Florian Königsmann <70534154+Zwergenland27@users.noreply.github.com>
2024-11-08 15:56:01 +0100

    Improve automate table by using local functions

--- Pl0/Automata.cs
+++ Pl0/Automata.cs
@@ -5,6 +5,7 @@ public class Automata : IDisposable
     private StreamReader _reader;
 
     private int _currentState = 0;
+    private int _followState = 0;
     private bool _finished = false;
     private Morph? _currentMorph;
     
@@ -54,26 +55,58 @@ public class Automata : IDisposable
 
         return characterClass;
     }
-
+    
     //TODO: Implement the automata table using delegates to the automate methods read, writeRead etc. and add the followState as int parameter
     //[State][CharacterClass as int]
-    private AC[][] _automataTable =
-    [
-        /*State Special    Digit     Letter     Colon     Equal      LessThan  GreaterThan Other*/
-        /* 0*/ [AC.WRE(0), AC.WR(1), AC.WUR(2), AC.WR(3), AC.WRE(0), AC.WR(4), AC.WR(5), AC.E(0)],
-        /* 1*/ [AC.E(1),   AC.WR(1), AC.E(1),   AC.E(1),  AC.E(1),   AC.E(1),  AC.E(1),  AC.E(1)],
-        /* 2*/ [AC.E(2),   AC.WR(2), AC.WUR(2), AC.E(2),  AC.E(2),   AC.E(2),  AC.E(2),  AC.E(2)],
-        /* 3*/ [AC.E(3),   AC.E(3),  AC.E(3),   AC.E(3),  AC.WR(6),  AC.E(3),  AC.E(3),  AC.E(3)],
-        /* 4*/ [AC.E(4),   AC.E(4),  AC.E(4),   AC.E(4),  AC.WR(7),  AC.E(4),  AC.E(4),  AC.E(4)],
-        /* 5*/ [AC.E(5),   AC.E(5),  AC.E(5),   AC.E(5),  AC.WR(8),  AC.E(5),  AC.E(5),  AC.E(5)],
-        /* 6*/ [AC.E(6),   AC.E(6),  AC.E(6),   AC.E(6),  AC.E(6),   AC.E(6),  AC.E(6),  AC.E(6)],
-        /* 7*/ [AC.E(7),   AC.E(7),  AC.E(7),   AC.E(7),  AC.E(7),   AC.E(7),  AC.E(7),  AC.E(7)],
-        /* 8*/ [AC.E(8),   AC.E(8),  AC.E(8),   AC.E(8),  AC.E(8),   AC.E(8),  AC.E(8),  AC.E(8)],
-    ];
+    private Action[][] _automataTable;
 
     public Automata(String filePath)
     {
         _reader = new StreamReader(filePath);
+        _initTable();
+    }
+
+    private void _initTable()
+    {
+        //Write read exit states
+        void WRE0() => WRE(0);
+        
+        //Write read states
+        void WR1() => WR(1);
+        void WR2() => WR(2);
+        void WR3() => WR(3);
+        void WR4() => WR(4);
+        void WR5() => WR(6);
+        void WR6() => WR(6);
+        void WR7() => WR(7);
+        void WR8() => WR(8);
+        
+        //Write upper read states
+        void WUR2() => WUR(2);
+        
+        //Exit states
+        void E0() => E(0);
+        void E1() => E(1);
+        void E2() => E(2);
+        void E3() => E(3);
+        void E4() => E(4);
+        void E5() => E(5);
+        void E6() => E(6);
+        void E7() => E(7);
+        void E8() => E(8);
+        _automataTable =
+        [
+            /*State Spec  Dig  Let   Col  Equa  LT   GT   Ot*/
+            /* 0*/ [WRE0, WR1, WUR2, WR3, WRE0, WR4, WR5, E0],
+            /* 1*/ [E1,   WR1, E1,   E1,  E1,   E1,  E1,  E1],
+            /* 2*/ [E2,   WR2, WUR2, E2,  E2,   E2,  E2,  E2],
+            /* 3*/ [E3,   E3,  E3,   E3,  WR6,  E3,  E3,  E3],
+            /* 4*/ [E4,   E4,  E4,   E4,  WR7,  E4,  E4,  E4],
+            /* 5*/ [E5,   E5,  E5,   E5,  WR8,  E5,  E5,  E5],
+            /* 6*/ [E6,   E6,  E6,   E6,  E6,   E6,  E6,  E6],
+            /* 7*/ [E7,   E7,  E7,   E7,  E7,   E7,  E7,  E7],
+            /* 8*/ [E8,   E8,  E8,   E8,  E8,   E8,  E8,  E8],
+        ];
     }
 
     public Morph Lex()
@@ -85,25 +118,8 @@ public class Automata : IDisposable
         do
         {
             var nextState = _automataTable[_currentState][(int)_getCharacterClass(_currentChar)];
-            _currentState = nextState.FollowState;
-            switch (nextState.Action)
-            {
-                case AutomataAction.Read:
-                    _read();
-                    break;
-                case AutomataAction.Exit:
-                    _exit();
-                    break;
-                case AutomataAction.WriteUpperRead:
-                    _writeUpperRead();
-                    break;
-                case AutomataAction.WriteRead:
-                    _writeRead();
-                    break;
-                case AutomataAction.WriteReadExit:
-                    _writeReadExit();
-                    break;
-            }
+            _currentState = _followState;
+            nextState();
         } while (!_finished);
 
         if (_currentMorph is null)
@@ -112,6 +128,12 @@ public class Automata : IDisposable
         }
 
         return _currentMorph;
+    }
+
+    public void R(int followState)
+    {
+        _followState = followState;
+        _read();
     }
     
     private void _read()
@@ -130,6 +152,12 @@ public class Automata : IDisposable
         }
         _currentColumn++;
     }
+
+    public void E(int followState)
+    {
+        _followState = followState;
+        _exit();
+    }
     
     private void _exit()
     {
@@ -139,6 +167,12 @@ public class Automata : IDisposable
         }
         _currentMorph.Finish(_currentState);
         _finished = true;
+    }
+
+    public void WUR(int followState)
+    {
+        _followState = followState;
+        _writeUpperRead();
     }
     
     private void _writeUpperRead()
@@ -153,6 +187,12 @@ public class Automata : IDisposable
         _read();
     }
 
+    public void WR(int followState)
+    {
+        _followState = followState;
+        _writeRead();
+    }
+    
     private void _writeRead()
     {
         if (_currentMorph is null)
@@ -163,6 +203,12 @@ public class Automata : IDisposable
         _currentMorph.AppendChar(_currentChar);
 
         _read();
+    }
+
+    public void WRE(int followState)
+    {
+        _followState = followState;
+        _writeReadExit();
     }
     
     private void _writeReadExit()
